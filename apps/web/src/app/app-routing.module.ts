2c277ce00d959800860052953e97889ae0ca6750
nomikhokher <nomankhokher786@gmail.com>
2022-05-25 15:04:37 +0000

    sign-in page

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 /* APP - ROUTES */
 import { Authin } from './views/auth/authin/authin.component';
 import { Home } from './views/home/home.component';
+import { Auth } from './views/authin/auth/auth.component';
 
 
 
 const routes: Routes = [
   { path: '', component: Home },
   { path: 'authin', component: Authin },
+  { path: 'auth', component: Auth },
 ];
 
 @NgModule({
---
+++ apps/web/src/app/views/authin/auth/authin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class Auth implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
