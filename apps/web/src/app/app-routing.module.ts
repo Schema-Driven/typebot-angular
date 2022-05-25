a13461add924426c2f26b97fedfe1eb6ddfcb3c4
nomikhokher <nomankhokher786@gmail.com>
2022-05-25 18:40:12 +0000

    dashboard page

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 /* APP - ROUTES */
 import { Authin } from './views/auth/authin/authin.component';
 import { Home } from './views/home/home.component';
-import { Auth } from './views/authin/auth/auth.component';
+import { Login } from './views/login/login.component';
+import { Dashboard  } from './views/dashboard/dashboard.component';
 
 
 
 const routes: Routes = [
   { path: '', component: Home },
   { path: 'authin', component: Authin },
-  { path: 'auth', component: Auth },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
 ];
 
 @NgModule({
