2f7c6162fb28a807cfa722a82dad4121c512074f
nomikhokher <nomankhokher786@gmail.com>
2022-06-11 00:58:21 +0500

    [share.component]

--- apps/builder/src/app/views/share/share.component.ts
+++ apps/builder/src/app/views/share/share.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,10 +10,30 @@ export class Share implements OnInit {
   constructor(private router: Router) {
     // ...
   }
-
+  toggle: boolean = false;
   ngOnInit(): void {}
 
   navigate(links: any[]) {
     this.router.navigate(links);
   }
+  changeColor() {
+    this.toggle = true;
+  }
+  changeColor1() {
+    this.toggle = false;
+  }
+  copyInputMessage(inputElement: any, val: any) {
+    inputElement.select();
+    //document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+    //console.log(inputElement.value);
+  }
+  // changeValue(val:any) {
+  //   console.log(val.value);
+  //   val = 'copied';
+  //   setTimeout(function () {
+  //     val='copy';
+  //   }.bind(this), 5000);
+  //   console.log('test');
+  // };
 }
