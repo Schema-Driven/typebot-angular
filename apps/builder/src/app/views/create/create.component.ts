7e265d72667658de187b89d8d0d5a3eae1366c1c
nomikhokher <nomankhokher786@gmail.com>
2022-07-01 22:26:55 +0500

    editor|create popup

--- apps/builder/src/app/views/create/create.component.ts
+++ apps/builder/src/app/views/create/create.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'create',
@@ -11,14 +10,16 @@ export class Create implements OnInit {
   constructor(private router: Router) {
     // ...
   }
-  popup: boolean = false;
+
+  popupAct: boolean = false;
   ngOnInit(): void {}
 
   navigate(links: any[]) {
     this.router.navigate(links);
   }
+
   popUpSet() {
-    this.popup = !this.popup;
+    this.popupAct = !this.popupAct;
     console.log('popUpset function');
   }
   ClickedOut(event: any) {
@@ -27,7 +28,7 @@ export class Create implements OnInit {
       event.target.className ===
       'sec-popup flex items-end sm:items-center justify-center mt-14 p-4 text-center sm:p-0'
     ) {
-      this.popup = false;
+      this.popupAct = false;
       console.log('OuteSide Click');
     }
   }
