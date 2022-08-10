1284669a9f74e79dbe97d0d116f7c9744c28ba64
nomikhokher <nomankhokher786@gmail.com>
2022-08-11 00:20:31 +0500

    Added seperate test list for typebot

--- apps/builder/src/app/views/editor/drop-test/drop-test.component.ts
+++ apps/builder/src/app/views/editor/drop-test/drop-test.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem} from '@angular/cdk/drag-drop';
 
 /**
  * @title Drag&Drop connected sorting
@@ -14,10 +14,47 @@ export class DropTestComponent {
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
+  fixeditems = [
+    {
+      uuid: '123',
+      name: 'bubbles',
+      blocks: ['Haroon', 'Ali']
+    },
+    {
+      uuid: '324',
+      name: 'inputs',
+      blocks: ['Zia', 'Waqas']
+    }
+  ]
+
+  groupblock = [
+    {
+      name: "Block 1",
+      blocks: ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'],
+    },
+    {
+      name: "Block 2",
+      blocks: ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'],
+    }
+  ]
+
   drop(event: CdkDragDrop<string[]>, container: string) {
+    console.log("event", event);
     console.log("container", container);
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else if ( event.previousContainer.id === "fixed-list" && event.container.id === "receiving-list" ) {
+      copyArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else if ( event.previousContainer.id === "fixed-list" ) {
+      this.addNewContainer(event.previousContainer.data[event.previousIndex]);
+    } else if ( container === "container") {
+      this.addNewContainer(event.previousContainer.data[event.previousIndex]);
+      event.previousContainer.data.splice(event.previousIndex,1);
     } else {
       console.log("event.container.data", event.container.data)
       transferArrayItem(
@@ -27,5 +64,21 @@ export class DropTestComponent {
         event.currentIndex,
       );
     }
+  }
+
+  addNewContainer(newGroup: any) {
+    this.groupblock.push({
+      name: "Block " + (this.groupblock.length + 1),
+      blocks: [newGroup]
+    });
+  }
+
+  /** Predicate function that doesn't allow items to be dropped into a list. */
+  noReturnPredicate() {
+    return false;
+  }
+
+  allowDrag(event: CdkDrag<number>, container: string) {
+    return false;
   }
 }
