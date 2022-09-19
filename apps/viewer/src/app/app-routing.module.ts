3f8210d7c5f041ecb672da322e676fe7bce784b0
nomikhokher <nomankhokher786@gmail.com>
2022-09-19 18:48:56 +0500

    separate route for (editor-fields-data) viewer

--- apps/viewer/src/app/app-routing.module.ts
+++ apps/viewer/src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewChat } from './components/view_chat/view_chat.component';
 
-const routes: Routes = [{ path: 'viewchat', component: ViewChat }];
+const routes: Routes = [
+  { path: 'viewchat', component: ViewChat },
+  {
+    path: 'editor-fields-data',
+    loadChildren: () =>
+      import(
+        './components/fields-data/editor-fields-data/editor-fields-data.module'
+      ).then((m) => m.EditorFieldsDataModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
