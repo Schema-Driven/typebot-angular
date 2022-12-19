9a57d1ed9499dd434fb8580423228e89939c829f
nomikhokher <nomankhokher786@gmail.com>
2022-12-19 23:22:16 +0500

    --removed unused code

--- apps/builder/src/app/views/editor/editor.component.ts
+++ apps/builder/src/app/views/editor/editor.component.ts
@@ -88,10 +88,6 @@ export class EditorComponent extends Editor {
       }
     });
 
-    // this.editorService.selectedGroupBlocks$.subscribe((group)=>{
-    //   this.Group = group;
-    // })
-
 
     this.createInstance(this.wrapper);
 
